ff188ad2b3f2eeafa345afc336c8c3663f46a0ee
[dyad] <hamzaperso00@gmail.com>
2026-03-15 00:13:18 +0100

    [dyad] Updated for Vercel serverless functions - wrote 1 file(s)

--- server.js
+++ server.js
@@ -107,7 +107,5 @@ app.post('/compose', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export for Vercel serverless functions
+module.exports = app;
